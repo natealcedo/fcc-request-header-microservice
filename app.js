7fb21c75a98fb45467bd9a44be8e2201c7f5b6eb
nathaniel d alcedo jr <ndaljr@gmail.com>
2016-12-17 16:37:53 +0800

    Made the ip adress ipv4

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const pug = require('pug')
 const app = express()
 const port = process.env.PORT || 8080
 
-
+app.enable('trust proxy')
 
 // Routes
-app.get('/', (req,res)=>{
-    let ipAdress = req.connection.remoteAddress || req.headers['x-forwarded-for']
+app.get('/', (req, res) => {
+    let ipAdress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    if (ipAdress.substr(0, 7) == "::ffff:") {
+        ipAdress = ipAdress.substr(7)
+    }
     let obj = {
         ip_Address: ipAdress,
         software: req.headers['user-agent'].split(') ')[0].split(';')[1].trim(),
@@ -16,4 +19,6 @@ app.get('/', (req,res)=>{
     res.json(obj);
 })
 
-app.listen(port)
+app.listen(port, function () {
+    console.log('Listening on port ', port)
+})
